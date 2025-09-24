cd80206506888d045fa7e0d8e51b597008529613
Ivan <vanyacherneyko@gmail.com>
2025-09-24 17:11:15 +0300

    +files

--- auto-light.py
+++ auto-light.py
@@ -1,1 +1,12 @@
-
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+led=26
+datchik=31
+GPIO.setup(datchik, GPIO.IN)#как цифровой выход
+GPIO.setup(led, GPIO.OUT)
+while True:
+    light=GPIO.input(datchik)
+    if light:
+        GPIO.output(led,1)
+    time.sleep(0.2)
