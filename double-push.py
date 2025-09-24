f313468fc696d7d8f532807e5c6d2cdca6c1df34
Ivan <vanyacherneyko@gmail.com>
2025-09-24 18:17:46 +0300

    newer

---
+++ double-push.py
@@ -0,0 +1,26 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+leds=[16,12,25,17,27,23,22,24]
+pl=9
+mn=10
+GPIO.setup(leds, GPIO.OUT)
+GPIO.output(leds,0)
+GPIO.setup(pl, GPIO.IN)
+GPIO.setup(mn, GPIO.IN)
+time_sleep=0.2
+num=0
+def totwo(x):
+    return [int(element) for element in bin(x)[2:].zfill(8)]
+while True:
+    if GPIO.input(pl):
+        num+=1
+        num%=255
+    if GPIO.input(mn):
+        if num:
+            num-=1
+    if GPIO.input(mn) and GPIO.input(pl):   
+        num=255     
+    time.sleep(time_sleep)
+    for i in range(8):
+        GPIO.output(leds[i],totwo(num)[i])
