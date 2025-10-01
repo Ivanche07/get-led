47a97dfa2a5d5d17e6e59e1049eb72b26510b7f5
Ivan <vanyacherneyko@gmail.com>
2025-10-01 16:50:24 +0300

    problem in file

--- 8-bit-dac-manual.py
+++ 8-bit-dac-manual.py
@@ -1,7 +1,18 @@
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+leds=[16,12,25,17,27,23,22,24]
+GPIO.setup(leds, GPIO.OUT)
+GPIO.output(leds,0)
+dynamic_range=3.3
+
+def number_to_dac(x):
+    return [int(element) for element in bin(x)[2:].zfill(8)]
+
 def voltage_to_number(voltage):
     if not (0.0 <= voltage <= dynamic_range):
         print(f"Напряжение выходит за динамический диапазон ЦАП (0.00 - {dynamic_range:.2f} В)")
-        print("Устанавлниваем 0.0 В")
+        #print("Устанавлниваем 0.0 В почему")
+        print("Число на вход ЦАП: "+"0"+", биты: "+"[0, 0, 0, 0, 0, 0, 0, 0]")
         return 0
 
     return int(voltage / dynamic_range * 255)
@@ -11,10 +22,12 @@ try:
             voltage = float(input("Введите напряжение в Вольтах: "))
             number = voltage_to_number(voltage)
             number_to_dac(number)
-
+            
+            for i in range(8):
+                GPIO.output(leds[i],number_to_dac(number)[i])
         except ValueError:
             print("Вы ввели не число. Попробуйте ещё раз\n")
 
 finally:
-    GPIO.output(dac_bits, 0)
+    GPIO.output(leds, 0)
     GPIO.cleanup()
